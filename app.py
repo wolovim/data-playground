81eb2feb5c375db1138046fe40177f93825525d0
Marc Garreau <marcdgarreau@gmail.com>
2016-09-21 21:37:54 -0600

    Introduces templates

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
+
+@app.route('/')
+def index():
+  return render_template('index.html')
 
 @app.route('/titanic')
 def titanic():
-  return 'titanic data coming soon'
+  return render_template('titanic.html')
 
 if __name__ == '__main__':
   app.run()
