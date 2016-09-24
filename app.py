8063b5cac48d982d8941475ebd20c23132d839b2
Marc Garreau <marcdgarreau@gmail.com>
2016-09-24 10:00:59 -0600

    Adds details for classes by gender

--- app.py
+++ app.py
@@ -6,19 +6,23 @@ import seaborn as sns
 app = Flask(__name__)
 titanic_df = pd.read_csv('./data/titanic-training-set.csv')
 
-def build_titanic_genders():
-  sns.set(font="serif")
-  plot = sns.factorplot('Sex', data=titanic_df, kind='count')
-  plot.fig.savefig("static/titanic/gender.png")
-
 @app.route('/')
 def index():
   return render_template('index.html')
 
 @app.route('/titanic')
 def titanic():
-  build_titanic_genders()
+  build_titanic_demographics()
   return render_template('titanic.html', df=titanic_df)
+
+def build_titanic_demographics():
+  # Passengers by gender
+  plot = sns.factorplot('Sex', data=titanic_df, kind='count')
+  plot.fig.savefig('static/titanic/gender.png')
+
+  # Classes by gender
+  plot_2 = sns.factorplot('Pclass', data=titanic_df, hue='Sex', kind='count', legend_out=False)
+  plot_2.fig.savefig('static/titanic/class.png')
 
 if __name__ == '__main__':
   app.run()
