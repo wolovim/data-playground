22c465cb896f6ee57de70010a2b600d16e2be2a4
Marc Garreau <marcdgarreau@gmail.com>
2016-09-25 22:07:13 -0600

    Includes titanic survival analysis

--- app.py
+++ app.py
@@ -26,12 +26,12 @@ def build_titanic_demographics():
   plot_2.fig.savefig('static/titanic/class.png')
 
   # Add a column to identify male, female, or child
-  titanic_df['person'] = titanic_df[['Age', 'Sex']].apply(identify_children, axis=1)
-  plot_3 = sns.factorplot('Pclass', data=titanic_df, hue='person', kind='count', legend_out=False)
+  titanic_df['Person'] = titanic_df[['Age', 'Sex']].apply(identify_children, axis=1)
+  plot_3 = sns.factorplot('Pclass', data=titanic_df, hue='Person', kind='count', legend_out=False)
   plot_3.fig.savefig('static/titanic/class-with-child.png')
 
   # FacetGrid for age distribution by gender
-  plot_4 = sns.FacetGrid(titanic_df, hue="person",aspect=4)
+  plot_4 = sns.FacetGrid(titanic_df, hue="Person",aspect=4)
   plot_4.map(sns.kdeplot, 'Age', shade=True)
   oldest = titanic_df['Age'].max()
   plot_4.set(xlim=(0,oldest))
@@ -63,6 +63,37 @@ def build_titanic_demographics():
   titanic_df['Alone'].loc[titanic_df['Alone'] == 0] = 'Alone'
   plot_7 = sns.factorplot('Alone', data=titanic_df, kind='count')
   plot_7.fig.savefig('static/titanic/family.png')
+
+  # Survival rate
+  titanic_df['Survivor'] = titanic_df.Survived.map({0: "no", 1: "yes"})
+  plot_8 = sns.factorplot('Survivor', data=titanic_df, kind='count')
+  plot_8.fig.savefig('static/titanic/survival-rate.png')
+
+  # Survival rate: class
+  plot_9 = sns.factorplot('Pclass', 'Survived', data=titanic_df)
+  plot_9.fig.savefig('static/titanic/survival-class.png')
+
+  # Survival rate: class by gender
+  plot_10 = sns.factorplot('Pclass', 'Survived', data=titanic_df, hue='Person', legend_out=False)
+  plot_10.fig.savefig('static/titanic/survival-class-gender.png')
+
+  # Survival rate: age
+  plot_11 = sns.lmplot('Age', 'Survived', data=titanic_df)
+  plot_11.fig.savefig('static/titanic/survival-age.png')
+
+  # Survival rate: age by class
+  x_bins = [10, 20, 40, 60, 80]
+  plot_12 = sns.lmplot('Age', 'Survived', data=titanic_df, hue='Pclass', palette='winter', x_bins=x_bins, legend_out=False)
+  plot_12.fig.savefig('static/titanic/survival-age-class.png')
+
+  # Survival rate: age by class
+  x_bins = [10, 20, 40, 60, 80]
+  plot_13 = sns.lmplot('Age', 'Survived', data=titanic_df, hue='Sex', palette='winter', x_bins=x_bins, legend_out=False)
+  plot_13.fig.savefig('static/titanic/survival-age-gender.png')
+
+  # Survival rate: family
+  plot_14 = sns.factorplot('Alone', 'Survived', data=titanic_df)
+  plot_14.fig.savefig('static/titanic/survival-family.png')
 
 def identify_children(passenger):
   age,sex = passenger
