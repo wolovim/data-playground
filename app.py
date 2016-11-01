8d3fe68dcb3c441b0f78b785b39cd61a3e6c8967
Marc Garreau <marcdgarreau@gmail.com>
2016-10-31 23:03:43 -0600

    Builds linear regression plots

--- app.py
+++ app.py
@@ -7,14 +7,67 @@ import seaborn as sns
 sns.set_style('whitegrid')
 from pandas.io.data import DataReader
 from datetime import datetime
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
 
 
 app = Flask(__name__)
+ecommerce_df = pd.read_csv('./data/ecommerce-customers.csv')
 titanic_df = pd.read_csv('./data/titanic-training-set.csv')
 
 @app.route('/')
 def index():
   return render_template('index.html')
+
+@app.route('/linear-regression')
+def linear_regression():
+  build_ecommerce_analysis()
+  return render_template('linear-regression.html', df=ecommerce_df)
+
+def build_ecommerce_analysis():
+  # Time on website / spending ratio
+  plot_1 = sns.jointplot('Time on Website', 'Yearly Amount Spent', data=ecommerce_df)
+  plot_1.fig.savefig('static/linear/joint-website.png')
+  plt.close()
+
+  # Time on app / spending ratio
+  plot_2 = sns.jointplot('Time on App', 'Yearly Amount Spent', data=ecommerce_df)
+  plot_2.fig.savefig('static/linear/joint-app.png')
+  plt.close()
+
+  # Time on app / membership length ratio
+  plot_3 = sns.jointplot('Time on App', 'Length of Membership', data=ecommerce_df, kind='hex')
+  plot_3.fig.savefig('static/linear/joint-app-hex.png')
+  plt.close()
+
+  # pairplot correlations
+  plot_4 = sns.pairplot(ecommerce_df)
+  plot_4.fig.savefig('static/linear/pairplot.png')
+  plt.close()
+
+  # Time on app / membership length ratio
+  plot_5 = sns.lmplot('Length of Membership', 'Yearly Amount Spent', data=ecommerce_df)
+  plot_5.fig.savefig('static/linear/lm-membership.png')
+  plt.close()
+
+  # Train a linear regression model
+  X = ecommerce_df[['Avg. Session Length', 'Time on App', 'Time on Website', 'Length of Membership']]
+  y = ecommerce_df['Yearly Amount Spent']
+  X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
+  lm = LinearRegression()
+  lm.fit(X_train, y_train)
+  predictions = lm.predict(X_test)
+  plt.close()
+
+  # Model fit
+  plot_6 = plt.scatter(y_test, predictions)
+  plot_6.get_figure().savefig('static/linear/fit.png')
+  plt.close()
+
+  # Model residuals
+  plot_7 = sns.distplot((y_test - predictions), bins=50)
+  plot_7.figure.savefig('static/linear/residuals.png')
+  plt.close()
 
 @app.route('/stocks')
 def stocks():
